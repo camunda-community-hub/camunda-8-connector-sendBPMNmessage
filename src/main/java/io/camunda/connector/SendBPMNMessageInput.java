dd4f2f664692d3de4667c1aa332f9c739fe140e8
Pierre-Yves <pierre-yves.monnet@laposte.net>
2022-11-21 16:38:03 -0800

    Remove all dependency to Cherry

--- src/main/java/io/camunda/connector/SendBPMNMessageInput.java
+++ src/main/java/io/camunda/connector/SendBPMNMessageInput.java
@@ -1,20 +1,11 @@
 package io.camunda.connector;
 
-import io.camunda.cherry.definition.AbstractConnectorInput;
-import io.camunda.cherry.definition.RunnerParameter;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
 
-public class SendBPMNMessageInput extends AbstractConnectorInput {
-    private static final String INPUT_MESSAGE_NAME = "messageName";
-    private static final String INPUT_CORRELATION_VARIABLES = "correlationVariables";
-    private static final String INPUT_MESSAGE_VARIABLES = "messageVariables";
-    private static final String INPUT_MESSAGE_ACCESS = "*";
-    private static final String INPUT_MESSAGE_ID_VARIABLES = "messageId";
-    private static final String INPUT_MESSAGE_DURATION = "messageDuration";
+public class SendBPMNMessageInput {
+
 
     @NotEmpty
     private String messageName;
@@ -59,20 +50,5 @@ public class SendBPMNMessageInput extends AbstractConnectorInput {
 
     }
 
-    /**
-     * Return the parameters definition
-     *
-     * @return list of parameters
-     */
-    @Override
-    public List<RunnerParameter> getInputParameters() {
-        return Arrays.asList(
-                RunnerParameter.getInstance(INPUT_MESSAGE_NAME, "Message name", String.class, RunnerParameter.Level.REQUIRED, "Message name"),
-                RunnerParameter.getInstance(INPUT_CORRELATION_VARIABLES, "Correlation variables", String.class, RunnerParameter.Level.OPTIONAL, "Correlation variables. The content of theses variable is used to find the process instance to unfroze"),
-                RunnerParameter.getInstance(INPUT_MESSAGE_VARIABLES, "Message variables", String.class, RunnerParameter.Level.OPTIONAL, "Variables to copy in the message"),
-                RunnerParameter.getInstance(INPUT_MESSAGE_ID_VARIABLES, "ID message", String.class, RunnerParameter.Level.OPTIONAL, "Id of the message"),
-                RunnerParameter.getInstance(INPUT_MESSAGE_ACCESS, "*", String.class, RunnerParameter.Level.OPTIONAL, "Access any variables referenced in the Correlation of the Message list"),
-                RunnerParameter.getInstance(INPUT_MESSAGE_DURATION, "Duration (FEEL duration, String like 'PT1S', or time in ms)", Object.class, RunnerParameter.Level.OPTIONAL, "Message duration. After this delay, message is deleted if it doesn't fit a process instance"));
 
-    }
 }
